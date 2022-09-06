1e3ec9b10f78f1e8d1b1e5e3d1197fc9a95e7192
Yaraud <107685910+Yaraud@users.noreply.github.com>
2022-09-06 21:23:49 +0300

    Delete app directory

--- app/src/main/java/com/hfad/budda/BuddhaApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.hfad.budda
-
-import android.app.Application
-import com.hfad.budda.database.AppDatabase
-
-class BuddhaApplication: Application() {
-    val database: AppDatabase by lazy { AppDatabase.getDatabase(this)}
-}
